ed6190d2dbdf71f5f254ac7276ccbec303d07200
DW-wen <dwen0630@gmail.com>
2026-01-01 11:09:04 +0000

    add new test data

--- test/TWSE_mocker.py
+++ test/TWSE_mocker.py
@@ -145,13 +145,13 @@ def create_packet_useless_format():
 
     return esc_code + header + body + checksum + terminal_code
 
-def create_packet_format_23_1():
+def create_packet_format_23_OTC():
     # ESC-CODE (ASCII 27)
     esc_code = bytes([0x1B]) 
     
     # HEADER
     header = bytes([
-        0x00, # 2.1 Message Length Not Finished
+        0x00, # 2.1 Message Length
         0x67,
         0x02, # 2.2 Business Type
         0x23, # 2.3 Format Code
@@ -186,6 +186,57 @@ def create_packet_format_23_1():
     terminal_code = b'\x0D\x0A'
     return esc_code + header + body + checksum + terminal_code
 
+def create_packet_format_23_TWSE():
+    # ESC-CODE (ASCII 27)
+    esc_code = bytes([0x1B]) 
+    
+    # HEADER 
+    header = bytes([
+        0x00, # 2.1 Message Length 
+        0x94, 
+        0x01, # 2.2 Business Type 
+        0x23, # 2.3 Format Code
+        0x01, # 2.4 Format Version
+        0x00, # 2.5 Transmission Number
+        0x31, 
+        0x24, 
+        0x55  
+        ])
+    
+    # BODY
+    body = (
+        b'\x32\x33\x33\x30\x20\x20' # 3.1 Stock code: "2330  "
+        b'\x13\x15\x30\x99\x88\x77' # 3.2 Match time: 13:15:30.998.877
+        b'\xB2'                     # 3.3 Display Flag: Deal(1), Bids(3), Asks(1) -> 1 011 001 0
+        b'\x00'                     # 3.4 Limit Up/Limit Down Flag: Normal
+        b'\x10'                     # 3.5 Status Flag: Normal
+        b'\x00\x00\x00\x10\x50\x00' # 3.6 Cumulative Volume: 1,050,000
+        
+        # 3.7 Dynamic Prices(5) and Quantities(6)
+        # --- Deal ---
+        b'\x00\x05\x85\x50\x00'     # trade price: 585.5000
+        b'\x00\x00\x00\x00\x00\x10' # trade quantity: 10
+        
+        # --- Bids ---
+        b'\x00\x05\x85\x00\x00'     # buy price 1: 585.0000
+        b'\x00\x00\x00\x00\x01\x00' # buy quantity 1: 100
+        b'\x00\x05\x84\x50\x00'     # buy price 2: 584.5000
+        b'\x00\x00\x00\x00\x05\x00' # buy quantity 2: 500
+        b'\x00\x05\x84\x00\x00'     # buy price 3: 584.0000 
+        b'\x00\x00\x00\x00\x08\x00' # buy quantity 3: 800
+        
+        # --- Asks ---
+        b'\x00\x05\x86\x00\x00'     # sell price 1: 586.0000
+        b'\x00\x00\x00\x00\x07\x00' # sell quantity 1: 700
+    )
+    
+    # Calculate checksum
+    checksum = bytes([calculate_checksum(header + body)])
+    
+    # TERMINAL-CODE
+    terminal_code = b'\x0D\x0A'
+    return esc_code + header + body + checksum + terminal_code
+
 def create_packet_invalid():
     return create_packet_1()[::-1]
 
@@ -205,7 +256,7 @@ if __name__ == "__main__":
 
     packets = [
         create_packet_1(), create_packet_2(), create_packet_3(), create_packet_4(),
-        create_packet_useless_format(), create_packet_invalid(), create_packet_format_23_1()
+        create_packet_useless_format(), create_packet_invalid(), create_packet_format_23_OTC(), create_packet_format_23_TWSE()
     ]
     packet_index = 0
 
